7f4e66a64c51c0157902db4c889e263718f9beb9
hjpunzalan <hj.punzalan@hotmail.com>
2023-01-03 23:58:00 +0800

    add item in basket entity

--- API/Entities/Basket.cs
+++ API/Entities/Basket.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace API.Entities
 {
@@ -8,8 +9,10 @@ namespace API.Entities
         public string BuyerId { get; set; }
         public List<BasketItem> Items { get; set; } = new();
 
-        public void AddItem(Product product, int Quantity) {
-            
+        public void AddItem(Product product, int quantity) {
+            if (Items.All(item=> item.ProductId != product.Id)) {
+                Items.Add(new BasketItem { Product = product, Quantity = quantity });
+            }
         }
     }
 }
