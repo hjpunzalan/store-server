69bb7dba2faa89f00290d53116f6ddbe254a2836
hjpunzalan <hj.punzalan@hotmail.com>
2023-01-03 23:59:11 +0800

    add Item fix

--- API/Entities/Basket.cs
+++ API/Entities/Basket.cs
@@ -13,6 +13,9 @@ namespace API.Entities
             if (Items.All(item=> item.ProductId != product.Id)) {
                 Items.Add(new BasketItem { Product = product, Quantity = quantity });
             }
+
+            var existingItem = Items.FirstOrDefault(item=> item.ProductId == product.id);
+            if (existingItem != null) existingItem.Quantity += quantity;
         }
     }
 }
