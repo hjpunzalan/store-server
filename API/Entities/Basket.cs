131d3e5af8929559c560f479617d04b49afbb69d
hjpunzalan <hj.punzalan@hotmail.com>
2023-01-03 23:56:27 +0800

    wip Basket entity

--- API/Entities/Basket.cs
+++ API/Entities/Basket.cs
@@ -1,8 +1,15 @@
+using System.Collections.Generic;
+
 namespace API.Entities
 {
     public class Basket
     {
         public int Id { get; set; }
         public string BuyerId { get; set; }
+        public List<BasketItem> Items { get; set; } = new();
+
+        public void AddItem(Product product, int Quantity) {
+            
+        }
     }
 }
