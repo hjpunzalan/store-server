a66ad859a6f7d30b41003412c1233129567dae8f
hjpunzalan <hj.punzalan@hotmail.com>
2023-10-30 21:59:23 +0800

    creating token service

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -10,16 +10,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    public class BasketController:BaseApiController
+    public class BasketController : BaseApiController
     {
         private readonly StoreContext _context;
         public BasketController(StoreContext context)
         {
             this._context = context;
-            
+
         }
 
-        [HttpGet(Name="GetBasket")]
+        [HttpGet(Name = "GetBasket")]
         public async Task<ActionResult<BasketDto>> GetBasket()
         {
             var basket = await RetrieveBasket();
@@ -29,37 +29,40 @@ namespace API.Controllers
         }
 
 
-        [HttpPost] 
-        public async Task<ActionResult<BasketDto>> AddItemToBasket (int productId, int quantity) {
+        [HttpPost]
+        public async Task<ActionResult<BasketDto>> AddItemToBasket(int productId, int quantity)
+        {
             // get basket
             var basket = await RetrieveBasket();
-            if (basket == null) {
+            if (basket == null)
+            {
                 basket = CreateBasket();
             }
 
             // get product
             var product = await this._context.Products.FindAsync(productId);
-            if (product ==  null) return BadRequest(new ProblemDetails{ Title="Product Not Found" });
+            if (product == null) return BadRequest(new ProblemDetails { Title = "Product Not Found" });
 
             // add item 
             basket.AddItem(product, quantity);
             // save changes
             var result = await this._context.SaveChangesAsync() > 0;
-            if (result)    return CreatedAtRoute("GetBasket", MapBasketToDto(basket));
+            if (result) return CreatedAtRoute("GetBasket", MapBasketToDto(basket));
             return BadRequest(new ProblemDetails { Title = "Problem saving item to basket" });
         }
 
         [HttpDelete]
-        public async Task<ActionResult>  RemoveBasketItem(int productId, int quantity) {
+        public async Task<ActionResult> RemoveBasketItem(int productId, int quantity)
+        {
             // get basket
             var basket = await RetrieveBasket();
-            if (basket==null) return NotFound();
+            if (basket == null) return NotFound();
 
             // remove item or reduce quanitty,
             basket.RemoveItem(productId, quantity);
             // Save changes
             var result = await this._context.SaveChangesAsync() > 0;
-            if (result)    return StatusCode(204);
+            if (result) return StatusCode(204);
             return BadRequest(new ProblemDetails { Title = "Problem removing item from basket" });
         }
 
@@ -72,14 +75,14 @@ namespace API.Controllers
             return basket;
         }
 
-        
+
         private Basket CreateBasket()
         {
             var buyerId = Guid.NewGuid().ToString();
-            var cookieOptions = new CookieOptions {IsEssential = true, Expires = DateTime.Now.AddDays(30) };
+            var cookieOptions = new CookieOptions { IsEssential = true, Expires = DateTime.Now.AddDays(30) };
 
             Response.Cookies.Append("buyerId", buyerId, cookieOptions);
-            var basket = new Basket{BuyerId= buyerId};
+            var basket = new Basket { BuyerId = buyerId };
             this._context.Baskets.Add(basket);
 
             return basket;
@@ -104,6 +107,6 @@ namespace API.Controllers
             };
         }
 
-        
+
     }
 }
