3bc46f6c54583792a53e4f4dd4e36ef3ab0a7aa0
hjpunzalan <hj.punzalan@hotmail.com>
2023-11-30 23:47:05 +0800

    fix basket login bug

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -54,7 +55,7 @@ namespace API.Controllers
       {
         Email = user.Email,
         Token = await _tokenService.GenerateToken(user),
-        Basket = anonBasket != null ? anonBasket.MapBasketToDto() : userBasket.MapBasketToDto()
+        Basket = anonBasket != null ? anonBasket.MapBasketToDto() : userBasket?.MapBasketToDto()
       };
     }
 
@@ -85,11 +86,13 @@ namespace API.Controllers
     public async Task<ActionResult<UserDto>> GetCurrentUser()
     {
       var user = await _userManager.FindByNameAsync(User.Identity.Name);
+      var userBasket = await RetrieveBasket(user.UserName);
 
       return new UserDto
       {
         Email = user.Email,
-        Token = await _tokenService.GenerateToken(user)
+        Token = await _tokenService.GenerateToken(user),
+        Basket = userBasket.MapBasketToDto()
       };
     }
 
