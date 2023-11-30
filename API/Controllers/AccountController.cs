26777c7c3131c845164562081a244a8c4655d2f8
hjpunzalan <hj.punzalan@hotmail.com>
2023-11-30 23:54:29 +0800

    fixing current user fetch basket

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -86,13 +86,13 @@ namespace API.Controllers
     public async Task<ActionResult<UserDto>> GetCurrentUser()
     {
       var user = await _userManager.FindByNameAsync(User.Identity.Name);
-      var userBasket = await RetrieveBasket(user.UserName);
+      var userBasket = await RetrieveBasket(User.Identity.Name);
 
       return new UserDto
       {
         Email = user.Email,
         Token = await _tokenService.GenerateToken(user),
-        Basket = userBasket.MapBasketToDto()
+        Basket = userBasket?.MapBasketToDto()
       };
     }
 
