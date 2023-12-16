88bcf6e11d569a5b1b77c92e6319bc6d9f844618
hjpunzalan <hj.punzalan@hotmail.com>
2023-12-16 23:37:29 +0800

    basket extension order controller

--- API/Entities/OrderAggregate/ProductItemOrdered.cs
+++ API/Entities/OrderAggregate/ProductItemOrdered.cs
@@ -6,7 +6,7 @@ namespace API.Entities.OrderAggregate
   [Owned]
   public class ProductItemOrdered
   {
-    public string ProductId { get; set; }
+    public int ProductId { get; set; }
     public string Name { get; set; }
 
     public string PictureUrl { get; set; }
